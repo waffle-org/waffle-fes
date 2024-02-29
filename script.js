11b4afd382691dc3daa58587ec0764b2db224fff
hinaho <hi0201naho@gmail.com>
2024-03-01 01:49:13 +0900

    faq

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+$(".slider").slick({
+  autoplay: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: '<div class="slick-prev"></div>',
+  nextArrow: '<div class="slick-next"></div>',
+  centerMode: true,
+  variableWidth: true,
+  dots: true,
+});
 // const modal = document.getElementById("modal");
 
 // modal.addEventListener("shown.bs.modal", () => {
